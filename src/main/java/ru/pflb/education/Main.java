d46b8e4a6120d24f540d2a6df83adce6e26f1406
Ruslan Shikhov <shikhov.ru@yandex.ru>
2019-12-19 01:29:27 +0300

    Added methods for work with numbers

---
+++ src/main/java/ru/pflb/education/Main.java
@@ -0,0 +1,18 @@
+package ru.pflb.education;
+
+public class Main {
+
+    public String convertToBinary(int a){
+        return Integer.toBinaryString(a);
+    }
+    public String convertToHex(int a){
+        return Integer.toHexString(a);
+    }
+    public String convertToOctal(int a){
+        return Integer.toOctalString(a);
+    }
+
+    public static boolean checkNumber(String b){
+
+    }
+}
