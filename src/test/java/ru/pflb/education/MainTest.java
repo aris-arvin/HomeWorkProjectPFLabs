1b2da949d6382f315b8967c0ee789fac6c81bdaa
Ruslan Shikhov <shikhov.ru@yandex.ru>
2019-12-20 00:05:27 +0300

    Added changes for names of DataProvider's body in MainTest class. Also added negative test in MainTest class.

--- src/test/java/ru/pflb/education/MainTest.java
+++ src/test/java/ru/pflb/education/MainTest.java
@@ -1,5 +1,6 @@
 package ru.pflb.education;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -7,7 +8,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class MainTest {
-    Main main = new Main();
+    Main main;
 
     @BeforeClass
     public void setUp(){
@@ -20,7 +21,7 @@ public class MainTest {
     }
 
     @DataProvider(name = "LongToBinary")
-    public Object[][] dtb(){
+    public Object[][] longToBinary(){
         return new Object[][]{
                 {269, "100001101"},{568, "1000111000"},{10000, "10011100010000"}
         };
@@ -28,11 +29,12 @@ public class MainTest {
 
     @Test(description = "Checking parsing from decimal to binary", dataProvider = "LongToBinary")
     public void testBinary(long a, String exResult){
+
         Assert.assertEquals(main.convertToBinary(a), exResult);
     }
 
     @DataProvider(name = "LongToHex")
-    public Object[][] dth(){
+    public Object[][] longToHex(){
         return new Object[][]{
                 {300, "12c"},{3456, "d80"},{666, "29a"}
         };
@@ -44,7 +46,7 @@ public class MainTest {
     }
 
     @DataProvider(name = "BinaryToOctal")
-    public Object[][] bto(){
+    public Object[][] binaryToOctal(){
         return new Object[][]{
                 {"10000000", "200"},{"010011", "23"},{"100100110", "446"}
         };
@@ -52,11 +54,12 @@ public class MainTest {
 
     @Test(description = "Checking parsing from binary to oktal", dataProvider = "BinaryToOctal")
     public void testBinaryToOctal(String a, String exResult){
+
         Assert.assertEquals(main.convertToOctal(a), exResult);
     }
 
     @DataProvider(name = "StringIsNumber")
-    public Object[] sin(){
+    public Object[] stringIsNumber(){
         return new Object[]{
                 "0xff", "6653", "110110101011", "3BF16D41"
         };
@@ -67,8 +70,17 @@ public class MainTest {
         Assert.assertTrue(Main.checkNumber(str));
     }
 
+    // Nevative check
 
+    @DataProvider(name = "StringIsNotNumber")
+    public Object[] checkNegativeStrings(){
+        return new Object[]{
+                "", "advm", " 888 88", " ", "-345g", "gmg", "null", "nan"
+        };
+    }
 
-
-
+    @Test(description = "Checking String of Binary is not number", dataProvider = "StringIsNotNumber", expectedExceptions = NumberFormatException.class)
+    public void testNegativeBinaryStrings(String stringIsNotNumber){
+        main.convertToOctal(stringIsNotNumber);
+    }
 }
